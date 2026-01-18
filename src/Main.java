9740f7954fa5792066d3cf37c0c2adf04f11eb25
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-18 16:09:35 +0200

    thingu

--- src/Main.java
+++ src/Main.java
@@ -1,52 +1,101 @@
 public class Main {
     public static void main(String[] args){
 
-        Employee worker1 = new Employee("Bob Dylan", 31, 23500d);
-        System.out.println(worker1.getFullName() + " earns " + worker1.getSalary() + " a year.");
+        Book book1 = new Book(10, 50, 0, 5, 100, 5);
+
+        book1.displaySales();
+
 
     }
 }
 
-class Employee {
+class Book {
+    private int totalSold;
+    private double totalSales;
+    private double totalDiscounts;
+    private double itemCost;
+    private int quantity;
+    private int bulkDiscount;
 
-    private String fullName;
-    private int age;
-    private double salary;
-
-    public Employee(String fullName, int age, double salary) {
-        this.fullName = fullName;
-        this.age = age;
-        this.salary = salary;
+    public Book(int totalSold, double totalSales, double totalDiscounts, double itemCost, int quantity, int bulkDiscount) {
+        this.totalSold = totalSold;
+        this.totalSales = totalSales;
+        this.totalDiscounts = totalDiscounts;
+        this.itemCost = itemCost;
+        this.quantity = quantity;
+        this.bulkDiscount = bulkDiscount;
     }
 
-    public String getFullName() {
-        return fullName;
+    public int getBulkDiscount() {
+        return bulkDiscount;
     }
 
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
+    public void setBulkDiscount(int bulkDiscount) {
+        this.bulkDiscount = bulkDiscount;
     }
 
-    public int getAge() {
-        return age;
+    public int getQuantity() {
+        return quantity;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
     }
 
-    public double getSalary() {
-        return salary;
+    public double getItemCost() {
+        return itemCost;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
-}
-
-class WarehouseWorker extends Employee {
-    public WarehouseWorker(String fullName, int age, double salary) {
-        super(fullName, age, salary);
+    public void setItemCost(double itemCost) {
+        this.itemCost = itemCost;
     }
 
+    public double getTotalDiscounts() {
+        return totalDiscounts;
+    }
+
+    public void setTotalDiscounts(double totalDiscounts) {
+        this.totalDiscounts = totalDiscounts;
+    }
+
+    public double getTotalSales() {
+        return totalSales;
+    }
+
+    public void setTotalSales(double totalSales) {
+        this.totalSales = totalSales;
+    }
+
+    public int getTotalSold() {
+        return totalSold;
+    }
+
+    public void setTotalSold(int totalSold) {
+        this.totalSold = totalSold;
+    }
+
+    public void registerSales(int n){
+        if(n <= 0){
+            System.out.println("Invalid item count");;
+        }
+        double currentSale = 0;
+        double currentDiscount = 0;
+
+        if ( n >= this.quantity) {
+            currentSale = n * this.itemCost * (1 - this.bulkDiscount);
+            currentDiscount = (n * this.itemCost) - currentSale;
+        } else {
+            currentSale = n * this.itemCost;
+        }
+
+        this.totalSales += currentSale;
+        this.totalSold += n;
+        this.totalDiscounts += currentDiscount;
+    }
+
+    public void displaySales(){
+        System.out.println("Total items sold: " + this.totalSold);
+        System.out.println("Gross profit: " + this.totalSales);
+        System.out.println("Total discout: " + this.totalDiscounts);
+    }
 }
