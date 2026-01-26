06589d0632dba7c02ae398ed8ae83354af9c5333
RRyannis <137493198+RRyannis@users.noreply.github.com>
2026-01-26 11:13:51 +0200

    random exercise

--- src/Main.java
+++ src/Main.java
@@ -2,50 +2,70 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        SalesAssociate[] workforce = new SalesAssociate[3];
-        for (int i = 0; i < workforce.length; i++){
-            workforce[i] = new SalesAssociate("Jane Doe", 5000);
+//        SalesAssociate[] workforce = new SalesAssociate[3];
+//        for (int i = 0; i < workforce.length; i++){
+//            workforce[i] = new SalesAssociate("Jane Doe", 5000);
+//        }
+//        for (SalesAssociate salesperson: workforce){
+//            salesperson.writeOutput();
+//        }
+        int[] days = new int[10];
+        for (int count = 0; count < days.length; count++){
+            Scanner scanner = new Scanner(System.in);
+            days[count] = scanner.nextInt();
         }
-        for (SalesAssociate salesperson: workforce){
-            salesperson.writeOutput();
+        double averageTemp = 0;
+        int daysAboveAverage = 0;
+        double sum = 0;
+
+        for (int day : days) {
+            sum += day;
         }
+        averageTemp = sum / 10;
+
+        for (int day : days) {
+            if (day >= averageTemp){
+                daysAboveAverage++;
+            }
+        }
+        System.out.println("The number of days with above average temperature was: " + daysAboveAverage);
     }
 }
 
-class SalesAssociate{
-    private String name;
-    private double sales;
-
-    public SalesAssociate(String name, double sales){
-        this.name = name;
-        this.sales = sales;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSales() {
-        return sales;
-    }
-
-    public void setSales(double sales) {
-        this.sales = sales;
-    }
-    public void readInput(){
-        System.out.print("Enter the name of a sales associate: ");
-        Scanner scanner = new Scanner(System.in);
-        name = scanner.nextLine();
-
-        System.out.print("Enter the associate's sales record: ");
-        sales = scanner.nextDouble();
-    }
-    public void writeOutput(){
-        System.out.println("Name: " + name);
-        System.out.println("Sales: " + sales);
-    }
-}
+//class SalesAssociate{
+//    private String name;
+//    private double sales;
+//
+//    public SalesAssociate(String name, double sales){
+//        this.name = name;
+//        this.sales = sales;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public double getSales() {
+//        return sales;
+//    }
+//
+//    public void setSales(double sales) {
+//        this.sales = sales;
+//    }
+//    public void readInput(){
+//        System.out.print("Enter the name of a sales associate: ");
+//        Scanner scanner = new Scanner(System.in);
+//        name = scanner.nextLine();
+//
+//        System.out.print("Enter the associate's sales record: ");
+//        sales = scanner.nextDouble();
+//    }
+//    public void writeOutput(){
+//        System.out.println("Name: " + name);
+//        System.out.println("Sales: " + sales);
+//    }
+//}
